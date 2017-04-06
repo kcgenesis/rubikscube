359401a1b8af42fc6be7a44b3b4cda54c9c3887b
kirk <kirk.t.cameron@gmail.com>
2017-04-05 20:52:56 -0400

    button rotations working

--- cube/cube.js
+++ cube/cube.js
@@ -4,6 +4,19 @@ var canvas;
 var gl;
 var VERTICES_PER_CUBIE  = 36;
 var NUM_CUBIE = 2;
+var KEYCODE_q = 81;
+var KEYCODE_w = 87;
+var KEYCODE_e = 69;
+var KEYCODE_a = 65;
+var KEYCODE_s = 83;
+var KEYCODE_d = 68;
+var KEYCODE_z = 90;
+var KEYCODE_x = 88;
+var KEYCODE_c = 67;
+var KEYCODE_r = 82;
+var KEYCODE_f = 70;
+var KEYCODE_g=71;
+var KEYCODE_h=72;
 var points = [];
 var colors = [];
 var fColor;
@@ -14,20 +27,10 @@ var zAxis = 2;
 
 var axis = 0;
 var r_theta = [0,0,0];
-var r_planes = [
-  vec3(0,0,0),
-  vec3(0,0,0),
-  vec3(0,0,0)
-];
-var r_plane_pick = [
-  vec3(0,0,0),
-  vec3(0,0,0),
-  vec3(0,0,0)
-];
+
 var program;
 
-var deg=null;
-var pos=null;
+var sign=1;
 var animating =null;
 
 var r_thetaLoc;
@@ -49,10 +52,9 @@ const white = vec4(1.0, 1.0, 1.0, 1.0);
 var near = -20;
 var far = 40;
 var radius = 15.0;
-var theta  = 0.0;
-var phi    = 0.0;
 var dr = 5.0 * Math.PI/180.0;
-theta = phi += 2*dr;
+var theta,phi;
+theta = phi = 2*dr;
 
 var left = -3.0;
 var right = 3.0;
@@ -62,6 +64,7 @@ var bottom = -3.0;
 var modeViewMatrix, projectionMatrix;
 var modelViewMatrixLoc, projectionMatrixLoc;
 var vBuffer,vPosition;
+
 var myCube;
 window.onload = function init()
 {
@@ -70,16 +73,8 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    //var cubie1 = new Cubie();
-    //var cubie2 = new Cubie(1.1,0,-2);
 
-    //cubie1.create();
-    //cubie2.create();
-
-    //var myCube = new Cubie_plane();
-    //myCube.create();
-
-
+    myCube=new Cube();
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
@@ -117,131 +112,13 @@ window.onload = function init()
     projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
 
 
+
     //event listeners for buttons
+    myCube.cube_render();
+    console.log("curr_rot after render: ");
+    console.log(myCube.curr_rot);
 
-    document.getElementById( "xButton" ).onclick = function () {
-      var planes = mat3(
-        1,1,1,
-        0,0,0,
-        0,0,0
-      );
-      rotate(planes,1);
-    };
-    document.getElementById( "yButton" ).onclick = function () {
-      var planes = mat3(
-        0,0,0,
-        1,1,1,
-        0,0,0
-      );
-      rotate(planes,1);
-    };
-    document.getElementById( "zButton" ).onclick = function () {
-      var planes = mat3(
-        0,0,0,
-        0,0,0,
-        1,1,1
-      );
-      rotate(planes,1);
-    };
-    document.getElementById( "nxButton" ).onclick = function () {
-      var planes = mat3(
-        1,1,1,
-        0,0,0,
-        0,0,0
-      );(planes,-1);
-    };
-    document.getElementById( "nyButton" ).onclick = function () {
-      var planes = mat3(
-        0,0,0,
-        1,1,1,
-        0,0,0
-      );
-      rotate(planes,-1);
-    };
-    document.getElementById( "nzButton" ).onclick = function () {
-      var planes = mat3(
-        1,0,0,
-        0,0,0,
-        1,1,1
-      );
-      rotate(planes,-1);
-    };
-
-    document.getElementById( "rplane1" ).onclick = function () {
-
-      var planes = mat3(
-        1,0,0,
-        0,0,0,
-        0,0,0
-      );
-      rotate(planes,1);
-    };
-    document.getElementById( "rplane2" ).onclick = function () {
-      var planes = mat3(
-        0,1,0,
-        0,0,0,
-        0,0,0
-      );
-      rotate(planes,1);
-    };
-    document.getElementById( "rplane3" ).onclick = function () {
-      var planes = mat3(
-        0,0,1,
-        0,0,0,
-        0,0,0
-      );
-      rotate(planes,1);
-    };
-    document.getElementById( "rplane4" ).onclick = function () {
-      var planes = mat3(
-        0,0,0,
-        1,0,0,
-        0,0,0
-      );
-      rotate(planes,1);
-    };
-    document.getElementById( "rplane5" ).onclick = function () {
-      var planes = mat3(
-        0,0,0,
-        0,1,0,
-        0,0,0
-      );
-      rotate(planes,1);
-    };
-    document.getElementById( "rplane6" ).onclick = function () {
-      var planes = mat3(
-        0,0,0,
-        0,0,1,
-        0,0,0
-      );
-      rotate(planes,1);
-    };
-    document.getElementById( "rplane7" ).onclick = function () {
-      var planes = mat3(
-        0,0,0,
-        0,0,0,
-        1,0,0
-      );
-      rotate(planes,1);
-    };
-    document.getElementById( "rplane8" ).onclick = function () {
-      var planes = mat3(
-        0,0,0,
-        0,0,0,
-        0,1,0
-      );
-      rotate(planes,1);
-    };
-    document.getElementById( "rplane9" ).onclick = function () {
-      var planes = mat3(
-        0,0,0,
-        0,0,0,
-        0,0,1
-      );
-      rotate(planes,1);
-    };
-
-
+    document.addEventListener('keydown',keycontrol);
 
     document.getElementById( "Button1" ).onclick = function () {change_view(true,false,1);};
     document.getElementById( "Button2" ).onclick = function () {change_view(true,false,-1);};
@@ -250,14 +127,89 @@ window.onload = function init()
     document.getElementById( "Button7" ).onclick = function () {randomize();};
 
 
-    myCube = new Cube();
-    myCube.render();
 }
 //creates rubiks cube at the origin
+function keycontrol(event){
 
+  if(((event.keyCode == KEYCODE_q)
+  ||(event.keyCode == KEYCODE_w)
+  ||(event.keyCode == KEYCODE_e)
+  ||(event.keyCode == KEYCODE_a)
+  ||(event.keyCode == KEYCODE_s)
+  ||(event.keyCode == KEYCODE_d)
+  ||(event.keyCode == KEYCODE_z)
+  ||(event.keyCode == KEYCODE_x)
+  ||(event.keyCode == KEYCODE_c)
+  ||(event.keyCode == KEYCODE_r)
+  ||(event.keyCode == KEYCODE_f)
+  ||(event.keyCode == KEYCODE_g)
+  ||(event.keyCode == KEYCODE_h)) && (animating!=1)) {
+    myCube.curr_rot=[
+       vec3(0,0,0),
+       vec3(0,0,0),
+       vec3(0,0,0)
+     ];
+    if(event.keyCode == KEYCODE_q) {
+      myCube.curr_rot[0][0]=90;
+    }
+    else if(event.keyCode == KEYCODE_w) {
+      myCube.curr_rot[0][1]=90;
+    }
+    else if(event.keyCode == KEYCODE_e) {
+      myCube.curr_rot[0][2]=90;
+    }
+    else if(event.keyCode == KEYCODE_a) {
+      myCube.curr_rot[1][0]=90;
+    }
+    else if(event.keyCode == KEYCODE_s) {
+      myCube.curr_rot[1][1]=90;
+    }
+    else if(event.keyCode == KEYCODE_d) {
+      myCube.curr_rot[1][2]=90;
+    }
+    else if(event.keyCode == KEYCODE_z) {
+      myCube.curr_rot[2][0]=90;
+    }
+    else if(event.keyCode == KEYCODE_x) {
+      myCube.curr_rot[2][1]=90;
+    }
+    else if(event.keyCode == KEYCODE_c) {
+      myCube.curr_rot[2][2]=90;
+    }else if(event.keyCode == KEYCODE_f) {
+      myCube.curr_rot[0][0]=myCube.curr_rot[0][1]=myCube.curr_rot[0][2]=90;
+    }else if(event.keyCode == KEYCODE_g) {
+      myCube.curr_rot[1][0]=myCube.curr_rot[1][1]=myCube.curr_rot[1][2]=90;
+    }else if(event.keyCode == KEYCODE_h) {
+      myCube.curr_rot[2][0]=myCube.curr_rot[2][1]=myCube.curr_rot[2][2]=90;
+    }else if(event.keyCode == KEYCODE_r){
+      sign = -1;
+      console.log("sign: "+sign);
+    }
+
+    for(var i=0;i<myCube.curr_rot.length;i++){
+      for (var j=0;j<myCube.curr_rot[i].length;j++){
+        myCube.curr_rot[i][j] *= sign;
+      }
+    }
+
+
+    cube_rotate();
+  }
+
+}
 class Cube {
   constructor(){
     this.cubies = [];
+    this.curr_rot = [
+      vec3(0,0,0),
+      vec3(0,0,0),
+      vec3(0,0,0)
+    ];
+    this.r_planes =[
+      vec3(0,0,0),
+      vec3(0,0,0),
+      vec3(0,0,0)
+    ];
     this.t = [
       vec3( -1, -1,  1),
       vec3(  0, -1,  1),
@@ -287,14 +239,13 @@ class Cube {
       vec3(  0,  1, -1),
       vec3(  1,  1, -1)
     ];
-
-
     for(var i=0;i<this.t.length;i++){
         var c = new Cubie(this.t[i]);
         this.cubies.push(c);
     }
+
   }
-  render(){
+  cube_render(){
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     for(var i=0;i<this.cubies.length;i++){
       points = [];
@@ -303,14 +254,53 @@ class Cube {
       }
       r_theta =[];
       for(var j=0;j<3;j++){
-        //where's the cube located in the original cube
-        //use that to find which axis it belongs to
-        r_theta.push(r_planes[j][this.t[i][j]+1]);
+
+        r_theta.push(this.r_planes[j][this.t[i][j]+1]);
 
       }
+
       render();
     }
+  }
 
+}
+function cube_rotate(){
+  if(animating==1){
+    console.log("cant rotate");
+    return;
+  }else{
+    console.log("curr_rot in handler");
+    console.log(myCube.curr_rot);
+    cube_rotate_step();
+  }
+}
+function cube_rotate_step(){
+  console.log("animating "+animating);
+  animating=1;
+  var inc=10;
+  var nonzero=false;
+  for(var i=0;i<myCube.curr_rot.length;i++){
+    for(var j=0;j<myCube.curr_rot[i].length;j++){
+      if(myCube.curr_rot[i][j] != 0){
+        nonzero=true;
+        if((myCube.curr_rot[i][j] < 0)&&(inc>0)){
+          inc *= -1;
+        }else if((myCube.curr_rot[i][j] > 0)&&(inc<0)){
+          inc *= -1;
+        }
+        myCube.r_planes[i][j] += inc;
+        myCube.curr_rot[i][j] -= inc;
+        if(myCube.r_planes[i][j] >= 360) {myCube.r_planes[i][j] -= 360;}
+        else if(myCube.r_planes[i][j] < 0) {myCube.r_planes[i][j] += 360;}
+      }
+    }
+  }
+  if(nonzero){
+    myCube.cube_render();
+    requestAnimFrame(cube_rotate_step);
+  }else{
+    animating=0;
+    console.log("done nimating");
   }
 }
 //creates subcube at optional pos or origin
@@ -368,33 +358,6 @@ class Cubie {
 function randomize(){
 
 }
-
-/*
-  a cubie's r_theta is defined from the ORIGINAL position of the cube's planes.
-  we need to keep track of the state of the cubes in the application layer.
-
-  we should be able to say: what cubes are at locations = [l1,l2,l3];
-  where l1 is an x,y,z translation transformation.
-
-  we render each cube one by one, in a set order.
-  each cube's r_theta will be updated every time the matrix rotates.
-
-  depending on the cube's existing rotation, it will behave differently trying to rotate it further.
-
-  when rotating once in x,
-
-*/
-function rotate(planes,arg_sign){
-  if(animating==1){
-    console.log("cant rotate");
-    return;
-  }else{
-    console.log("rotating");
-    r_plane_pick = transpose(planes);
-    pos = arg_sign;
-    cube_rotate();
-  }
-}
 function change_view(theta_arg,phi_arg,sign){
   if(theta_arg){
     theta += sign*dr;
@@ -403,32 +366,14 @@ function change_view(theta_arg,phi_arg,sign){
     phi += sign*dr;
   }
   if((theta_arg!=false)||(phi_arg!=false)){
-    myCube.render();
+    myCube.cube_render();
   }
 }
-function cube_rotate(){
-  animating=1;
-  if(!deg){deg=0.0;}
-  var inc = pos*10.0;
-  deg += inc;
-  for(var i=0;i<r_planes.length;i++){
-    for(var j=0;j<r_planes[i].length;j++){
-      if(r_plane_pick[i][j]==1){
-        r_planes[i][j] += inc;
-        if(r_planes[i][j] >= 360) {r_planes[i][j] -= 360;}
-        else if(r_planes[i][j] < 0) {r_planes[i][j] += 360;}
-      }
-    }
-  }
-  myCube.render();
-  if (pos*deg<90) {
-    requestAnimFrame(cube_rotate);
-  }else{
-    deg=null;
-    animating=0;
-    //console.log(r_planes);
-  }
-}
+
+
+
+
+
 /*
   plane rotation ==> plane remapping
 
@@ -462,6 +407,7 @@ function cube_rotate(){
 
 function render()
 {
+
     //gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
@@ -477,15 +423,15 @@ function render()
 
     var eye = vec3(radius*Math.sin(phi), radius*Math.sin(theta),
          radius*Math.cos(phi));*/
-         console.log("theta: "+theta+"\nphi:"+phi);
+         //console.log("theta: "+theta+"\nphi:"+phi);
          var eyex,eyey,eyez;
          eyex=radius*Math.cos(phi)*Math.cos(theta);
          eyey=radius*Math.cos(phi)*Math.sin(theta);
          eyez=radius*Math.sin(phi);
          var eye = vec3(eyex,eyey,eyez);
-        console.log("eye: ["+eyex + ","+eyey+","+eyez+"]");
+        //console.log("eye: ["+eyex + ","+eyey+","+eyez+"]");
 
-
+//console.log("whattup!!");
     var modelViewMatrix = lookAt( eye, at, up );
     var projectionMatrix = ortho( left, right, bottom, ytop, near, far );
     gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
@@ -501,4 +447,5 @@ function render()
         gl.uniform4fv(fColor, flatten(black));
         gl.drawArrays( gl.LINE_LOOP, i, 4 );
     }
+
 }
